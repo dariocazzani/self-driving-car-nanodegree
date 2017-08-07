9809d12c39eb907ee9d0afdd26ced30c3401e7d7
Dario Cazzani <dariocazzani@gmail.com>
2017-08-06 18:00:29 -0700

    Started Traffic sign classification problem

---
+++ CarND-Traffic-Sign-Classifier-Project-master/start_docker.sh
@@ -0,0 +1,5 @@
+docker run --privileged --rm -it \
+    -v $PWD:/root/CarND-CarND-Traffic-Sign-Classifier-Project-master-P1 \
+    -w /root/CarND-CarND-Traffic-Sign-Classifier-Project-master-P1 \
+    -p 8888:8888 \
+    carnd:term1 bash
