916ba38aa648e926604484949155f6a7b9c9f182
Dario Cazzani <dcazzani@cisco.com>
2017-08-18 16:03:45 -0700

    First scripts for Transfer Learning lessons

---
+++ 11-Transfer-learning/download_data_and_code.sh
@@ -0,0 +1,12 @@
+# Download code
+wget https://github.com/udacity/CarND-Alexnet-Feature-Extraction/archive/master.zip
+unzip master.zip
+mv CarND-Alexnet-Feature-Extraction-master/* .
+rm -r CarND-Alexnet-Feature-Extraction-master
+rm master.zip
+
+# Download data
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
+
+# Download pretrained weights
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
---
+++ 11-Transfer-learning/start_docker.sh
@@ -0,0 +1,4 @@
+docker run --privileged --rm -it \
+    -v $PWD:/root/11-Transfer-Learning \
+    -w /root/11-Transfer-Learning \
+    ubuntu:selfcar bash
