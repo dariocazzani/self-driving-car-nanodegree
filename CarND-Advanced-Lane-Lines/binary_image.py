80b80bbcb2ca7bd64108587383507a64108add86
Dario Cazzani <dcazzani@cisco.com>
2017-09-21 07:31:03 -0700

    Use l channel

--- CarND-Advanced-Lane-Lines/binary_image.py
+++ CarND-Advanced-Lane-Lines/binary_image.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-def get_binary_img(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+def get_binary_img(img, s_thresh=(120, 255), sx_thresh=(20, 255),l_thresh=(40,255)):
     img = np.copy(img)
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float)
@@ -22,16 +22,20 @@ def get_binary_img(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     # Threshold color channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
-    # Stack each channel
-    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
-    # be beneficial to replace this channel with something else.
-    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
 
-    # Combine the two binary thresholds
-    combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
-    combined_binary = np.dstack((combined_binary, combined_binary, combined_binary)) * 255
-    return combined_binary, color_binary
+     # Threshold lightness
+    l_binary = np.zeros_like(l_channel)
+    l_binary[(l_channel >= l_thresh[0]) & (l_channel <= l_thresh[1])] = 1
+
+    # Stack each channel
+    channels = 255*np.dstack(( l_binary, sxbinary, s_binary)).astype('uint8')
+
+    # Combine the three binary thresholds
+    binary = np.zeros_like(sxbinary)
+    binary[((l_binary == 1) & (s_binary == 1) | (sxbinary==1))] = 1
+    binary = 255*np.dstack((binary,binary,binary)).astype('uint8')
+
+    return binary, channels
 
 if __name__ == '__main__':
     num_test_images = 6
