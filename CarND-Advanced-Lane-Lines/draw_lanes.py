2fdbba14454b315a895ba0862a3929114462ff6b
Dario Cazzani <dcazzani@cisco.com>
2017-09-22 17:32:00 -0600

    Refactored and show redius and offseton image

--- CarND-Advanced-Lane-Lines/draw_lanes.py
+++ CarND-Advanced-Lane-Lines/draw_lanes.py
@@ -4,20 +4,21 @@ from camera_calib import get_calib_points, get_coefficients, undistort_image
 from binary_image import get_binary_img
 from perspective_transform import get_matrix_transform
 from find_lanes import get_lanes_full_search, get_binary_birdeye
+from curvature import get_curvatures_and_offset
 
-def draw_lane(input_image):
-    img_size = (input_image.shape[0], input_image.shape[1])
-    # Get camera matrix and distortion coefficients
-    objpoints, imgpoints = get_calib_points()
-    # get perspective transformation matrix
-    _, M, Minv = get_matrix_transform()
+input_image = cv2.imread('test_images/test1.jpg')
+img_size = (input_image.shape[0], input_image.shape[1])
+# Get camera matrix and distortion coefficients
+objpoints, imgpoints = get_calib_points()
+# get perspective transformation matrix
+_, M, Minv = get_matrix_transform()
 
-    ret, mtx, dist, rvecs, tvecs = get_coefficients(objpoints, imgpoints, img_size)
-    undist = undistort_image(input_image, mtx, dist)
+ret, mtx, dist, rvecs, tvecs = get_coefficients(objpoints, imgpoints, img_size)
 
-    warped = get_binary_birdeye(input_image)
 
+def draw_lane(warped, undist):
     lefty, leftx, righty, rightx, out_img = get_lanes_full_search(warped)
+    left_curverad, right_curverad, center_offset_meters = get_curvatures_and_offset(lefty, leftx, righty, rightx, warped)
     # Fit a second order polynomial to each
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
@@ -43,11 +44,20 @@ def draw_lane(input_image):
     newwarp = cv2.warpPerspective(color_warp, Minv, (undist.shape[1], undist.shape[0]))
     # Combine the result with the original image
     result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    # Add info about radius and offset
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text1 = 'left_curverad {:.2f} meters - right_curverad: {:.2f}'.format(left_curverad, right_curverad)
+    text2 = 'offset: {:.3f} meters'.format(center_offset_meters)
+    cv2.putText(result, text1, (50, 30), font, 1, (0,0,255), 1, cv2.LINE_AA)
+    cv2.putText(result, text2, (50, 70), font, 1, (0,0,255), 1, cv2.LINE_AA)
     return result
 
 if __name__ == '__main__':
+
     num_test_images = 6
     for test_image in range(1, num_test_images+1):
         input_image = cv2.imread('test_images/test{}.jpg'.format(test_image))
-        result = draw_lane(input_image)
+        undist = undistort_image(input_image, mtx, dist)
+        warped = get_binary_birdeye(input_image)
+        result = draw_lane(warped, undist)
         cv2.imwrite('output_images/test{}_with_drawn_lanes.jpg'.format(test_image), result)
